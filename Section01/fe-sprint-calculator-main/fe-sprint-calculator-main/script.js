a743a8e09241df56b6a2b17c192d3d7b305af71f
Dragonmin 0918 <dragonmin0918@Dragonminui-MacBookPro.local>
2023-07-02 07:18:16 +0900

    Update

---
+++ Section01/fe-sprint-calculator-main/fe-sprint-calculator-main/script.js
@@ -0,0 +1,164 @@
+const calculator = document.querySelector(".calculator");
+const buttons = calculator.querySelector(".calculator__buttons");
+const firstOperend = document.querySelector(".calculator__operend--left");
+const operator = document.querySelector(".calculator__operator");
+const secondOperend = document.querySelector(".calculator__operend--right");
+const calculatedResult = document.querySelector(".calculator__result");
+
+let operatorForAdvanced = "";
+
+function calculate(n1, operator, n2) {
+  let result = 0;
+  if (operator === "+") {
+    result = Number(n1) + Number(n2);
+  }
+  if (operator === "-") {
+    result = Number(n1) - Number(n2);
+  }
+  if (operator === "*") {
+    result = Number(n1) * Number(n2);
+  }
+  if (operator === "/") {
+    result = Number(n1) / Number(n2);
+  }
+  return String(result);
+}
+
+buttons.addEventListener("click", function (event) {
+  const target = event.target;
+  const action = target.classList[0];
+  const buttonContent = target.textContent;
+
+  if (target.matches("button")) {
+    if (action === "number") {
+      if (operatorForAdvanced === "") {
+        if (firstOperend.textContent === "0") {
+          firstOperend.textContent = buttonContent;
+        } else {
+          firstOperend.textContent += buttonContent;
+        }
+      } else {
+        if (secondOperend.textContent === "0") {
+          secondOperend.textContent = buttonContent;
+        } else {
+          secondOperend.textContent += buttonContent;
+        }
+      }
+    } else if (action === "operator") {
+      operator.textContent = buttonContent;
+      operatorForAdvanced = buttonContent;
+    } else if (action === "decimal") {
+      if (operatorForAdvanced === "") {
+        if (!firstOperend.textContent.includes(".")) {
+          firstOperend.textContent += ".";
+        }
+      } else {
+        if (!secondOperend.textContent.includes(".")) {
+          secondOperend.textContent += ".";
+        }
+      }
+    } else if (action === "clear") {
+      firstOperend.textContent = "0";
+      secondOperend.textContent = "0";
+      operator.textContent = "+";
+      calculatedResult.textContent = "0";
+      operatorForAdvanced = "";
+    } else if (action === "calculate") {
+      const result = calculate(
+        firstOperend.textContent,
+        operatorForAdvanced,
+        secondOperend.textContent
+      );
+      calculatedResult.textContent = result;
+      firstOperend.textContent;
+      secondOperend.textContent;
+      operator.textContent;
+      operatorForAdvanced;
+    }
+  }
+});
+
+// ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
+
+const display = document.querySelector(".calculator__display--for-advanced"); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+
+let firstNum = "";
+let operatorForBegin = "";
+let previousKey = "";
+let previousNum = "";
+
+buttons.addEventListener("click", function (event) {
+  const target = event.target;
+  const action = target.classList[0];
+  const buttonContent = target.textContent;
+
+  if (target.matches("button")) {
+    if (action === "number") {
+      if (display.textContent === "0" && operatorForAdvanced === "") {
+        display.textContent = buttonContent;
+        firstNum = display.textContent;
+      } else if (display.textContent !== "0" && operatorForAdvanced === "") {
+        display.textContent += buttonContent;
+        firstNum = display.textContent;
+      } else if (display.textContent !== "0" && operatorForAdvanced !== "") {
+        if (previousKey === operatorForAdvanced) {
+          display.textContent = buttonContent;
+          previousKey = display.textContent;
+          previousNum = display.textContent;
+        } else if (previousKey !== operatorForAdvanced) {
+          display.textContent += buttonContent;
+          previousNum = display.textContent;
+        }
+      }
+    }
+
+    if (action === "operator") {
+      operatorForAdvanced = buttonContent;
+      previousKey = operatorForAdvanced;
+    }
+    if (action === "decimal") {
+      if (operatorForAdvanced === "") {
+        if (!display.textContent.includes(".")) {
+          display.textContent += ".";
+        }
+      } else if (operatorForAdvanced !== "") {
+        if (display.textContent.includes(".")) {
+          display.textContent;
+        }
+      }
+    }
+
+    if (action === "clear") {
+      display.textContent = "0";
+      firstNum = "";
+      previousNum = "";
+      operatorForAdvanced = "";
+      previousKey = "";
+    }
+    if (action === "calculate") {
+      if (firstNum !== "" && operatorForAdvanced === "") {
+        display.textContent = firstNum;
+      } else if (firstNum !== "" && previousNum === "") {
+        display.textContent = calculate(
+          display.textContent,
+          operatorForAdvanced,
+          display.textContent
+        );
+      } else if (previousKey === display.textContent) {
+        display.textContent = calculate(
+          firstNum,
+          operatorForAdvanced,
+          previousNum
+        );
+      } else if (previousKey !== display.textContent && previousNum !== "") {
+        display.textContent = calculate(
+          display.textContent,
+          operatorForAdvanced,
+          previousNum
+        );
+      } else if (previousKey !== display.textContent && previousNum === "") {
+        display.textContent = firstNum;
+      }
+    }
+  }
+});
